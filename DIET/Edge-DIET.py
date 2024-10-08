75a031e08b05b42a969d787b3e97a98f403c29f7
Nassim26 <Nassimbeladel026@gmail.com>
2024-10-08 18:22:55 +0200

    Update Edge-DIET.py

--- DIET/Edge-DIET.py
+++ DIET/Edge-DIET.py
@@ -57,7 +57,7 @@ class DatasetWithIndices(Dataset):
     def __len__(self):
         return len(self.dataset)
 
-training_data = torchvision.datasets.MNIST(
+training_data = torchvision.datasets.FashionMNIST(
     train=True, download=True, root="\data",
     transform=torchvision.transforms.Compose(transform)
 )
@@ -67,7 +67,7 @@ if limit_data < np.inf:
   training_data = Subset(training_data, indices)
 
 training_data = DatasetWithIndices(training_data)
-test_data = torchvision.datasets.MNIST(
+test_data = torchvision.datasets.FashionMNIST(
     train=False, download=False, root="\data",
     transform=torchvision.transforms.Compose(transform)
 )
