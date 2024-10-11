c37854dfa6275490cf6c5ca59579248d02420cac
Nassim26 <Nassimbeladel026@gmail.com>
2024-10-11 22:07:07 +0200

    Update QuantMNIST_DIET.py

--- DIET/QuantMNIST_DIET.py
+++ DIET/QuantMNIST_DIET.py
@@ -29,7 +29,7 @@ class UnnormalizeTransform:
 
 device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
-num_epoch = 10
+num_epoch = 15
 batch_size = 1024
 lr = 1e-3
 weight_decay = 0.05
