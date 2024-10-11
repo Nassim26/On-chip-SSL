bcaa7839c559d0fc93380b485cf0f4daa3665fd1
Nassim26 <Nassimbeladel026@gmail.com>
2024-10-11 22:31:49 +0200

    Update Edge-DIET.py

--- DIET/Edge-DIET.py
+++ DIET/Edge-DIET.py
@@ -88,9 +88,9 @@ def train(net):
   W_probe = torch.nn.Linear(embedding_dim, num_classes).to(device)
   W_diet = torch.nn.Linear(embedding_dim, output_size, bias=False).to(device)
 
-  optimizer = torch.optim.AdamW(
+  optimizer = torch.optim.SGD(
       list(net.parameters()) + list(W_probe.parameters()) + list(W_diet.parameters()),
-      lr=lr, weight_decay=weight_decay
+      lr=lr
   )
 
   criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.0)
