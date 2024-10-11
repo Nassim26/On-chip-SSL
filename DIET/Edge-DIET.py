84d7e8e93c7558f01e73a6085343bbc1b4486c6b
Nassim26 <Nassimbeladel026@gmail.com>
2024-10-11 21:14:04 +0200

    Update Edge-DIET.py

--- DIET/Edge-DIET.py
+++ DIET/Edge-DIET.py
@@ -145,14 +145,14 @@ def train(net):
 class MinimalNetwork(nn.Module):
     def __init__(self, inChannels):
         super(MinimalNetwork, self).__init__()
-        self.hiddenLayer0 = KRIAInterface_clipped.Conv2D_3x3(inChannels, 16, bias=True)
+        self.hiddenLayer0 = KRIAInterface.Conv2D_3x3(inChannels, 16, bias=True)
         self.actFunc = ReLU255()
         self.maxPool1 = nn.MaxPool2d((2, 2), stride = 1)
         self.maxPool2 = nn.MaxPool2d((2, 2), stride = 2)
-        self.hiddenLayer1 = KRIAInterface_clipped.Conv2D_3x3(16, 32, bias=True)
+        self.hiddenLayer1 = KRIAInterface.Conv2D_3x3(16, 32, bias=True)
         self.Flatten = nn.Flatten()
-        self.hiddenLayer2 = KRIAInterface_clipped.Conv2D_3x3(32, 64, bias=True)
-        self.hiddenLayer3 = KRIAInterface_clipped.Conv2D_3x3(64, 128, bias=True)
+        self.hiddenLayer2 = KRIAInterface.Conv2D_3x3(32, 64, bias=True)
+        self.hiddenLayer3 = KRIAInterface.Conv2D_3x3(64, 128, bias=True)
         self.embedding_dim = 8192
 
     def forward(self, x):
@@ -163,7 +163,7 @@ class MinimalNetwork(nn.Module):
         #x = self.actFunc(x)
         x = self.maxPool2(x)
         x = self.hiddenLayer2(x)
-        x = self.hiddenLayer3(x)
+        #x = self.hiddenLayer3(x)
         #x = self.actFunc(x)
         # x = self.maxPool1(x)
         # x = self.hiddenLayer3(x)
