b2f29e0b3c1aa045a34c866224441bd8ebaa2380
Nassim26 <Nassimbeladel026@gmail.com>
2024-12-18 15:10:49 -0500

    Config

--- Circular DIET/config.py
+++ Circular DIET/config.py
@@ -12,7 +12,7 @@ class Config:
     def __init__(self):
         self.num_epoch = 25
         self.batch_size = 1024
-        self.output_size = 2048
+        self.output_size = 25 * self.batch_size
         self.lr = 1e-3
         self.weight_decay = 0.05
         self.label_smoothing = 0.8
