638658579c6c6f18741b419197e7e0fbdbaf6232
Nassim26 <Nassimbeladel026@gmail.com>
2024-12-18 14:57:23 -0500

    Config change

--- Circular DIET/config.py
+++ Circular DIET/config.py
@@ -10,7 +10,7 @@ class UnnormalizeTransform:
 
 class Config:
     def __init__(self):
-        self.num_epoch = 20
+        self.num_epoch = 50
         self.batch_size = 1024
         self.output_size = 100
         self.lr = 1e-3
