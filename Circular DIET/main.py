a1710917a1c80bfa540e59fdffa94be1421874bc
Nassim26 <Nassimbeladel026@gmail.com>
2024-12-20 12:47:22 -0500

    test3

--- Circular DIET/main.py
+++ Circular DIET/main.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
     config = Config()
     #net = QuantizedNetwork(config.in_channels, config.input_shape).to(device)
     # --- 
-    # net = torchvision.models.resnet18()
-    # net.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
-    # net.maxpool = torch.nn.Identity()
-    # embedding_dim = net.fc.in_features
-    # net.fc = torch.nn.Identity()
+    net = torchvision.models.resnet18()
+    net.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2, bias=False)
+    net.maxpool = torch.nn.Identity()
+    embedding_dim = net.fc.in_features
+    net.fc = torch.nn.Identity()
     # ---
-    net = torch.load('cifar_params.pth')
+    state_dict = torch.load('cifar_params.pth')
+    net.parameters = state_dict
     net.to(device)
     train_DIET_standard(net, device, config, embedding_dim)
     # train(net, device, config, net.embedding_dim)
