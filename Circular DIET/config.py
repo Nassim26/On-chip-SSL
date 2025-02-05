dec9c36e33d3240860f55bf19e68680fa86e8530
Nassim26 <Nassimbeladel026@gmail.com>
2025-02-05 15:15:04 +0100

    train on CIFAR10 to obtain satisfactory performance - establish baseline on standard DIET

--- Circular DIET/config.py
+++ Circular DIET/config.py
@@ -11,7 +11,7 @@ class UnnormalizeTransform:
 
 class Config:
     def __init__(self):
-        self.num_epoch = 20
+        self.num_epoch = 200
         self.batch_size = 1024
         self.output_size = 512
         self.lr = 1e-3
